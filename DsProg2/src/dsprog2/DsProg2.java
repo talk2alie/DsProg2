dccad909be2ad83339c2740af2cb677727324535
Mohamed Pussah <talk2alie@outlook.com>
2015-04-11 12:49:37 -0400

    Modified application

---
+++ DsProg2/src/dsprog2/DsProg2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsprog2;
+
+/**
+ *
+ * @author talk2_000
+ */
+public class DsProg2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Working");
+        System.out.println("Still working");
+    }
+    
+}
